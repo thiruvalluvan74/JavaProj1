7296144e5343c25982fe1890a270a4c16e277f50
thiruvalluvan74 <34278459+thiruvalluvan74@users.noreply.github.com>
2020-04-14 22:43:50 +0530

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -3,5 +3,6 @@ public static void main(String args[]) throws InterruptedException{
 Thread.sleep(10000);
 System.out.println("Hello Java");
 System.out.println("This added newly within the branch");
+System.out.println("This added @GitHub");
 }
 }

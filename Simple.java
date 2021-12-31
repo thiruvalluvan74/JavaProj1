8029c3da84e8a4f8ba3ca214d1f5a39ec94debb8
thiruvalluvan74 <34278459+thiruvalluvan74@users.noreply.github.com>
2021-12-31 16:23:15 +0530

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -1,8 +1,7 @@
 class Simple{
 public static void main(String args[]) throws InterruptedException{
 Thread.sleep(10000);
-System.out.println("Hello Java");
+System.out.println("Hello Java from Git Repo");
 System.out.println("This added newly within the branch");
-System.out.println("This added @GitHub");
 }
 }
